0858824c7e525dfd14edd94e022df0ec85646126
Kseniya Kuzmina <kseniya.kuzmina@devexpress.com>
2021-09-01 11:56:40 +0300

    update

--- CS/MapCacheOptions/MainWindow.xaml.cs
+++ CS/MapCacheOptions/MainWindow.xaml.cs
@@ -1,16 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Net;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace MapCacheOptions {
     /// <summary>
@@ -19,6 +8,7 @@ namespace MapCacheOptions {
     public partial class MainWindow : Window {
         public MainWindow() {
             InitializeComponent();
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
         }
     }
 }
