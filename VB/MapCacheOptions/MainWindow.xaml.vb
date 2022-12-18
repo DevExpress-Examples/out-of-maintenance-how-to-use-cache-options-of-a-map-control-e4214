7efd538d5032581fd10372433f3be77040040386
DevExpressExampleBot <fakeEmail>
2022-12-18 21:25:24 +0400

    Source auto update [skip ci]

--- VB/MapCacheOptions/MainWindow.xaml.vb
+++ VB/MapCacheOptions/MainWindow.xaml.vb
@@ -1,20 +1,21 @@
-﻿Imports System.Net
+Imports System.Net
 Imports System.Windows
 
 Namespace MapCacheOptions
-	''' <summary>
-	''' Interaction logic for MainWindow.xaml
-	''' </summary>
-	Partial Public Class MainWindow
-		Inherits Window
 
-		Public Sub New()
-			InitializeComponent()
-			ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12
-		End Sub
+    ''' <summary>
+    ''' Interaction logic for MainWindow.xaml
+    ''' </summary>
+    Public Partial Class MainWindow
+        Inherits Window
 
-		Private Sub OpenStreetMapDataProvider_WebRequest(ByVal sender As Object, ByVal e As DevExpress.Xpf.Map.MapWebRequestEventArgs)
-			e.UserAgent = "DevExpress OpenStreetMapProvider example"
-		End Sub
-	End Class
+        Public Sub New()
+            Me.InitializeComponent()
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12
+        End Sub
+
+        Private Sub OpenStreetMapDataProvider_WebRequest(ByVal sender As Object, ByVal e As DevExpress.Xpf.Map.MapWebRequestEventArgs)
+            e.UserAgent = "DevExpress OpenStreetMapProvider example"
+        End Sub
+    End Class
 End Namespace
